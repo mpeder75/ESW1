1eabb214214b41cffdefcbf232ad41d7ef7af680
Mpeder <mpeder75@gmail.com>
2023-02-26 13:36:31 +0100

    bbb

--- Aflevering1-postfixCalculator/src/calculator/postFixCalculator.java
+++ Aflevering1-postfixCalculator/src/calculator/postFixCalculator.java
@@ -2,6 +2,8 @@ package calculator;
 
 import linkedListQueue.LinkedListQueueADT;
 import token.IToken;
+import token.ITokenOperand;
+import token.TokenOperator;
 
 import java.util.ArrayList;
 
@@ -10,15 +12,53 @@ public class postFixCalculator implements IpostfixCalculatorADT {
     @Override
     public int evaluateExpression(ArrayList<IToken> tokenList) {
 
+        int a = 0;
+        int b = 0;
+
         LinkedListQueueADT<IToken> returnStack = new LinkedListQueueADT<>();
 
         for (IToken index : tokenList) {
-            if (index.getClass() == Character.class) {
-                return 0;
+
+            if (index instanceof ITokenOperand) {
+                returnStack.push(index);
+            }
+
+            if (index instanceof TokenOperator) {
+                switch (((TokenOperator) index).getValue()) {
+                    case '+':
+                        returnStack.push(new ITokenOperand(add((Integer) returnStack.pop().getValue(), (Integer) returnStack.pop().getValue())));
+                        break;
+                    case '-':
+                        returnStack.push(new ITokenOperand(subtract((Integer) returnStack.pop().getValue(), (Integer) returnStack.pop().getValue())));
+                        break;
+                    case '/':
+                        returnStack.push(new ITokenOperand(divide((Integer) returnStack.pop().getValue(), (Integer) returnStack.pop().getValue())));
+                        break;
+                    case '*':
+                        returnStack.push(new ITokenOperand(multiply((Integer) returnStack.pop().getValue(), (Integer) returnStack.pop().getValue())));
+                        break;
+                }
             }
         }
+        return (Integer) returnStack.pop().getValue();
 
     }
 
+
+    private int add(int a, int b) {
+        return a + b;
+    }
+
+    private int subtract(int a, int b) {
+        return a - b;
+    }
+
+    private int divide(int a, int b) {
+        return a / b;
+    }
+
+    private int multiply(int a, int b) {
+        return a * b;
+    }
 }
 
---
+++ Aflevering1-postfixCalculator/test/postFixTest.java
@@ -0,0 +1,16 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class postFixTest {
+
+
+   @Before
+    private void setup ()
+   {
+
+   }
+
+
+
+
+}
